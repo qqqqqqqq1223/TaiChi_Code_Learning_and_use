d58a6ed1e6004b9fb6f59344a22c5c62beb734ad
qqqqqqqq1223 <64718197+qqqqqqqq1223@users.noreply.github.com>
2022-09-22 22:40:23 +0800

    test2

--- Code Learning/demo1/demo1.py
+++ Code Learning/demo1/demo1.py
@@ -1,1 +1,27 @@
 ## 代码学习
+## taichi是一个编程语言，有自己的编译器
+## 用户可以将Taichi看作一个Python库
+## 大规模矩阵计算，数值模拟
+import taichi as ti
+import time
+ti.init(arch=ti.gpu)
+
+f = ti.field(dtype=ti.f32,shape=())
+
+# Taichi能够编译的 Taichi scope
+@ti.kernel
+def hello_taichi():
+    time_start = time.time()
+    f[None] = 0.0
+    print('Hello Taichi')
+    time_end = time.time()
+    print('Running time:{} seconds'.format(time_start - time_end))
+
+# Python编译
+def hello_pyrhon():
+    time_start = time.time()
+    print('Hello Python')
+    time_end = time.time()
+    print('Running time:{} seconds'.format(time_start - time_end))
+hello_taichi()
+hello_pyrhon()
